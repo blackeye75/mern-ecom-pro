eb0c6cccf6c5f4c874aed6d82793b728a30809f6
blackeye75 <priyanshuraj22275@gmail.com>
2026-01-15 18:40:24 +0530

    started

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = (req, res) => {
+  res.send("This is signup controller");
+}
+export const login = (req, res) => {
+  res.send("This is login controller");
+}
+export const logout = (req, res) => {
+  res.send("This is logout controller");
+}
